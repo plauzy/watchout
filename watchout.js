b706ef5bfbed85a6568e3ae758d83d3613c47f3b
plauer <plauer411@gmail.com>
2014-11-07 15:57:40 -0800

    draws 20 svg enemy circles

--- watchout.js
+++ watchout.js
@@ -1,2 +1,9 @@
 // start slingin' some d3 here.
 
+var canvas = d3.select("body").append("svg")
+              .attr({"width" : 1000, "height" : 1000});
+// var circle = canvas.append("circle").attr({"cx" : 100, "cy" : 100, "r" : 10});  
+var enemyData = new Array(20);
+var enemies = canvas.selectAll("circle").data(enemyData).enter()
+              .append("circle")
+              .attr({"cx" : function(d, i) {return i * 50}, "cy" : function(d, i){ return i * 50}, "r" : 10});
